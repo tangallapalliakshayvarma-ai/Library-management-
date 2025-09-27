aef462700b0be16815d0cc03ac40038ffdf68d9b
Tangallapalli Akshay Varma <tangallapalliakshayvarma.gmail.com>
2025-09-27 13:53:42 +0530

    Added new domain to ALLOWED_HOSTS

--- library_Management/settings.py
+++ library_Management/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = '90xr2q1k^5)1z_4f$jx1uj1&!simq32p7#yi2+t0v!e=lgy%#&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["library-management.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    'library-management.onrender.com',
+    'library-management-tyy9.onrender.com',
+    '127.0.0.1',
+    'localhost',
+]
+
 
 
 # Application definition
